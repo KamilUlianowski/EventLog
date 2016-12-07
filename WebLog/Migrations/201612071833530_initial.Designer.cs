a66ba74778f4fb9e738863d362819ccc8944ee6f
unknown <krzysq8989@gmail.com>
2016-12-07 21:48:54 +0100

    advertisement_css

--- WebLog/Migrations/201612071833530_initial.Designer.cs
+++ WebLog/Migrations/201612071833530_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace WebLog.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201612071143237_init"; }
+            get { return "201612071833530_initial"; }
         }
         
         string IMigrationMetadata.Source
